e2412630c36a9f0252cf528d8ec14619d464ad63
Migiefelizardo <miggyfelizardo@gmail.com>
2025-06-24 16:37:56 +0800

    change

--- server/Routes/commentRoutes.js
+++ server/Routes/commentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect, admin } = require('../middleware/auth');
+const { protect, admin } = require('../Middleware/auth');
 const { addComment, getCommentsByBlog, deleteComment } = require('../Controllers/commentController');
 
 router.post('/', protect, addComment);
